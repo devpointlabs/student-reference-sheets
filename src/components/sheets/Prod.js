31280091bb5b148d8bec2e66e0c2de5a6703bdcf
Henry Doan <henrydoan96@gmail.com>
2021-03-01 17:11:02 -0700

    prod stub

--- src/components/sheets/Prod.js
+++ src/components/sheets/Prod.js
@@ -105,7 +105,58 @@ const Prod = () => {
             </Accordion.Content>
           </Accordion>
           <br />
-          
+          <Accordion styled>
+            <Accordion.Title
+              active={netlifyCLIOpen}
+              onClick={() => setNetlifyCLIOpen(!netlifyCLIOpen)}
+            >
+              Netlify CLI
+              <Icon name='dropdown' />
+            </Accordion.Title>
+            <Accordion.Content active={netlifyCLIOpen}>
+              <p>
+                These steps below is for how to have a production app using the Netlify CLI in your terminal.
+              </p>
+              <p>
+                This site is good for static applications for just HTML sites and ReactJS sites. The home page is found on index.html so be sure you have one.
+              </p>
+              <p>
+                For ReactJS applications, to prevent a redirect error, we will create redirect rules, more about it here: <a href='https://docs.netlify.com/routing/redirects/' target='_blank' rel="noopener noreferrer">docs.netlify.com/routing/redirects/</a>
+              </p>
+              <p>
+                In the public folder create a _redirect file with this content below:
+              </p>
+              <code>/*    /index.html   200</code>
+              <br />
+              <br />
+              <p>
+                Login in to your account in the Netlify website, <a href='https://app.netlify.com' target='_blank' rel="noopener noreferrer">app.netlify.com</a>
+              </p>
+              <p>
+                After navigating to the Team overview or Sites tab, click on the teal button of New site from Git.
+              </p>
+              <b>Step 1. Pick A Git Provider</b>
+              <p>
+                Click on the git provider your project is on, and you might have to authorized the access or it will auto authorize for you.
+              </p>
+              <b>Step 2. Pick A Repository</b>
+              <p>
+                Use the search to find the repository you want in production.
+              </p>
+              <p>
+                Go with the default setting unless stated otherwised and press deploy site.
+              </p>
+              <p>
+                After your site is deployed, you can change the site and domain setting as desired. 
+              </p>
+              <p>
+                If there are any error, see if you can debug and resolve them in development, and to check production errors, go to the deploy tab and click on the failed build for more insight.
+              </p>
+              <p>
+                If you want to update your content, update the master branch on the GitHub repo and it will auto deploy.
+              </p>
+            </Accordion.Content>
+          </Accordion>
         </Grid.Column>
         <Grid.Column>
           <Accordion styled>
@@ -117,7 +168,16 @@ const Prod = () => {
               <Icon name='dropdown' />
             </Accordion.Title>
             <Accordion.Content active={herokuOpen}>
-             
+              <p>
+                One team member creates the initial project, push it up to Github with Initial Push Steps. Got into the Github repository settings and add all contributors. 
+              </p>
+              <p>
+                Contributors will accept the invite via email or Github. Then go into the Github repo and click on the Clone or Download button to copy the SSH URL.
+              </p>
+              <code>$ git clone *paste SSH URL</code>
+              <p>
+                Downloads the latest version from Github to local machine. 
+              </p>
             </Accordion.Content>
           </Accordion>
         </Grid.Column>
